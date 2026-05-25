26cccdcec10fbc2a1759bb21f2cf1770db359519
Escalona23 <s.escalona@duocuc.cl>
2026-05-25 14:18:41 -0400

    /commit validaciones

--- src/main/java/com/bookpoint/proyecto/model/Direccion.java
+++ src/main/java/com/bookpoint/proyecto/model/Direccion.java
@@ -1,27 +1,26 @@
 package com.bookpoint.proyecto.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "direcciones")
 public class Direccion {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "La calle no puede estar vacía")
+    @Size(min = 3, max = 50, message = "La calle debe tener entre 3 y 50 caracteres")
     @Column(nullable = false)
     private String calle;
 
+    @NotBlank(message = "La ciudad no puede estar vacía")
+    @Size(min = 3, max = 50, message = "La ciudad debe tener entre 3 y 50 caracteres")
     @Column(nullable = false)
     private String ciudad;
 
